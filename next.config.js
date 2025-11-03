4e5184381afa38f74b67f471cb049417d1c1ddca
Brendan Morrissey <brendanmorrissey34@gmail.com>
2025-11-03 17:19:31 -0500

    Configure GitHub Pages deployment for DevBranch

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/ggcmaps-fall25',
+  images: {
+    unoptimized: true
+  }
+};
+
 module.exports = nextConfig;

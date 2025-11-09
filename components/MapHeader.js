de172be8d1671edbc0860ce8dbf944170ad2db17
William Stein <thewillstein@gmail.com>
2025-11-09 13:48:38 -0500

    Header and footer height now matching

--- components/MapHeader.js
+++ components/MapHeader.js
@@ -63,7 +63,6 @@ export default function MapHeader() {
               {ui.mapHeader.helpHint}
             </span>
           )}
-          {/* Ensure copyright is on a new row */}
           <span className="text-muted small copyright">
             {ui.mapHeader.copyright}
           </span>

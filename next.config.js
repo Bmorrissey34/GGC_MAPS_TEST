d8845a29a6a7526743cb39e0177563717eff7ba8
Brendan Morrissey <brendanmorrissey34@gmail.com>
2025-11-19 19:32:59 -0500

    Fix basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/ggcmaps-fall25' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ggcmaps-fall25/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/GGC_MAPS_TEST' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/GGC_MAPS_TEST/' : '',
   images: {
     unoptimized: true
   }
